22c7ee7dabfc1f75f653028f28969c301be6b754
ishrat <ishrat.lallmamode@gmail.com>
2023-02-06 10:25:08 -0700

    initial commit

---
+++ src/Single/Chart.js
@@ -0,0 +1,23 @@
+import React from 'react';
+import { render } from 'react-dom';
+import Highcharts from 'highcharts';
+import HighchartsReact from 'highcharts-react-official';
+
+const options = {
+    chart: {
+      type: 'spline'
+    },
+    title: {
+      text: 'My chart'
+    },
+    series: [
+      {
+        data: [1, 2, 1, 4, 3, 6]
+      }
+    ]
+  };
+
+  function Charts() {
+    <div>    <HighchartsReact highcharts={Highcharts} options={options} />
+    </div>
+  }
---
+++ src/Single/Single.js
@@ -0,0 +1,10 @@
+import React from React; 
+import Container from '@mui/material/Container';
+
+
+
+function Single() {
+
+} 
+
+export default Single 
